54dbab871160e023b0097bb1e1419e31e271b807
josephcarr <54960330+josephcarr@users.noreply.github.com>
2020-12-06 18:22:21 -0500

    Update linked-list.h

--- linked-list.h
+++ linked-list.h
@@ -1,23 +1,29 @@
 #include "node.h"
 #include <vector>
 #include <string>
+#include <fstream>
 
 class LinkedList{
     private:
         Node* head;
+        int rows, cols;
         unsigned int size;
 
         void push_backR(Node* temp, int row, int col, int data);
         void to_stringR(Node* temp, std::string &listString);
+        void writeR(Node* temp, std::string outfile);
     public:
         LinkedList();
         LinkedList(int rows, int cols);
         ~LinkedList();
 
-        int rows, cols;
-
         Node* get_head();
+        int get_rows();
+        int get_cols();
         void push_back(int row, int col, int data);
+        void add(Node* matrixOne, Node* matrixTwo);
+        void multiply(const int& r, const int& c, Node* matrixOne, Node* matrixTwo);
         int get_size();
         std::string to_string();
+        void write(std::string outfile);
 };
