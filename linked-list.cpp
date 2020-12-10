86039b1517fc902889dea3311c3483feda6629b8
josephcarr <54960330+josephcarr@users.noreply.github.com>
2020-12-09 23:06:30 -0500

    Update linked-list.cpp

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,6 +1,3 @@
-#ifndef __LIST_H__
-#define __LIST_H__
-
 #include "linked-list.h"
 #include <iostream>
 
@@ -360,8 +357,4 @@ void LinkedList::write(std::string outfile) {
 
     return;
 }
-
-#endif
-
-
 
