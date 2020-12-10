b45e20e1b5147693fa9bc61da263686da22fbaf0
josephcarr <54960330+josephcarr@users.noreply.github.com>
2020-12-09 23:09:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,36 +3,11 @@
 #include <string.h>
 #include <vector>
 #include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <cstring>
+#include <ctime>
 #include "linked-list.h"
-
-void readFile(int& length, int& width, const std::string& infile, std::vector<int>& values) {
-    //read file to vector
-    std::ifstream file(infile);
-    std::string str, value;
-    while (std::getline(file, str)) {
-        // process line
-        for (int i = 0; i < str.size(); i++) {
-            if (str[i] != ' ') {
-                value += str[i];
-            }
-            else {
-                values.push_back(std::stoi(value));
-                value.clear();
-                width++;
-            }
-        }
-
-        values.push_back(std::stod(value));
-        value.clear();
-        width++;
-
-        length++;
-    }
-
-    width /= length;
-
-    return;
-}
 
 int main(int argc, char **argv) {
     //check for enough arguments
@@ -42,20 +17,14 @@ int main(int argc, char **argv) {
     }
 
     std::string fileOne = argv[1], fileTwo = argv[2], outfile = argv[3], operation = argv[4];
-    std::vector<int> valuesOne, valuesTwo;
-    int length = 0, width = 0, size;
+    int size;
     std::string matrixString;
 
     //read first file and creates Linked List based off of the rows and column
-    readFile(length, width, fileOne, valuesOne);
-    LinkedList matrixOne(length, width);
-
-    length = 0;
-    width = 0;
+    LinkedList matrixOne(fileOne);
 
     //read second file and creates Linked List based off of the rows and column
-    readFile(length, width, fileTwo, valuesTwo);
-    LinkedList matrixTwo(length, width);
+    LinkedList matrixTwo(fileTwo);
 
     //checks if matricies are same size for adddition
     if (operation == "a" && (matrixOne.get_rows() != matrixTwo.get_rows() || matrixOne.get_cols() != matrixTwo.get_cols())) {
@@ -71,39 +40,30 @@ int main(int argc, char **argv) {
     //creates a Linked List matrix for final operation
     LinkedList finalMatrix(matrixOne.get_rows(), matrixTwo.get_cols());
 
-    //pushes all none zero values from valuesOne to matrixOne
-    int count = 0;
-    for (int i = 0; i < matrixOne.get_rows(); i++) {
-        for (int j = 0; j < matrixOne.get_cols(); j++) {
-            if (valuesOne[count] != 0) {
-                matrixOne.push_back(i, j, valuesOne[count]);
-            }
-            count++;
-        }
-    }
+    std::cout << "Calculating..." << std::endl;
 
-    //pushes all none zero values from valuesTwo to matrixTwo
-    count = 0;
-    for (int i = 0; i < matrixTwo.get_rows(); i++) {
-        for (int j = 0; j < matrixTwo.get_cols(); j++) {
-            if (valuesTwo[count] != 0) {
-                matrixTwo.push_back(i, j, valuesTwo[count]);
-            }
-            count++;
-        }
-    }
+    uint64_t val;
+    unsigned long c_start, c_end;
 
     if (operation == "a") {
+        c_start = std::clock();
         finalMatrix.add(matrixOne.get_head(), matrixTwo.get_head());
+        c_end = std::clock();
     }
     else if (operation == "m") {
-        for (int r = 0; r < finalMatrix.get_rows(); r++) {
-            for (int c = 0; c < finalMatrix.get_cols(); c++) {
-                finalMatrix.multiply(r, c, matrixOne.get_head(), matrixTwo.get_head());
-            }
-        }
+        LinkedList tMatrix(matrixTwo.get_cols(), matrixTwo.get_rows());
+        tMatrix.transpose(matrixTwo.get_head());
+
+        c_start = std::clock();
+        finalMatrix.multiply(matrixOne.get_head(), tMatrix.get_head());
+        c_end = std::clock();
     }
 
+    float runTime = 1.0 * (c_end - c_start) / CLOCKS_PER_SEC;;
+
+    std::cout << "Done!" << std::endl;
+
+    
     //prints out the contents of matricies
     matrixString = matrixOne.to_string();
     std::cout << "Matrix One:" << "\n" << matrixString << std::endl;
@@ -115,6 +75,8 @@ int main(int argc, char **argv) {
     std::cout << "Matrix Final:" << "\n" << matrixString << std::endl;
 
     finalMatrix.write(outfile);
+
+    std::cout << "Run Time: " << runTime << std::endl;
 
     return 0;
 }
